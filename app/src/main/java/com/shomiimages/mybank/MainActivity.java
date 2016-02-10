03cd4cd24a2ce0c9e37bba606e3bdea7ef612789
TheChientleGiant <thechientlegiant@gmail.com>
2016-02-10 10:21:34 -0800

    Variable Creation

--- app/src/main/java/com/shomiimages/mybank/MainActivity.java
+++ app/src/main/java/com/shomiimages/mybank/MainActivity.java
@@ -8,8 +8,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText amountInput;
+    Button withdrawButton;
+    Button depositButton;
+    TextView amountDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
